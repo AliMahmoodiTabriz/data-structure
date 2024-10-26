739b4defafb49690e568fd49c870a557916e4849
ali mahmoodi <ali.mahmoodi.heris@gmail.com>
2024-10-26 15:58:02 +0300

    update code

--- Questions/BoatToSavePeople.py
+++ Questions/BoatToSavePeople.py
@@ -3,13 +3,19 @@ from typing import List
 
 
 def numRescueBoats(people: List[int], limit: int) -> int:
-        total_peaople=0
-        for item in people:
-            total_peaople += item
-        return math.ceil(total_peaople / limit) 
-    
-    
-    
-print(numRescueBoats([1,2],3))    
-print(numRescueBoats([3,2,2,1],3))    
-print(numRescueBoats([3,5,3,4],5))
+    people.sort()
+    boats = 0
+    left = 0
+    right= len(people) -1
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left +=1
+            right -= 1
+        else:
+            right -= 1
+        boats +=1    
+    return boats
+
+print(numRescueBoats([2,49,10,7,11,41,47,2,22,6,13,12,33,18,10,26,2,6,50,10], 50))
+print(numRescueBoats([1, 1, 2, 3], 3))
+print(numRescueBoats([1, 2, 3, 3], 5))
