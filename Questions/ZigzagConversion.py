f5a405f7f4d691f4fc04657a5d9e6f41bc1a23b9
ali <ali.mahmoodi.heris@gmail.com>
2024-11-11 10:42:07 +0300

    Update Questions

--- Questions/ZigzagConversion.py
+++ Questions/ZigzagConversion.py
@@ -1,26 +1,27 @@
 def convert(s: str, numRows: int) -> str:
-    lsStr = [""] * numRows
-    temp = numRows
-    r = 0
-    i = 0
     if numRows == 1:
         return s
-    while i <= len(s)-1:
-        lsStr[r] += s[i]
-        if r < temp-1:
-            r += 1
-        else:
-            if r == 0:
-                temp = numRows
-                r = 1
-            else:
-                r -= 1
-                temp -= 1
-        i += 1
-    result = ""
-    for item in lsStr:
-        result += item
-    return result
+
+    lsStr = [""] * numRows
+    r = 0
+    going_down = True
+    for item in s:
+        lsStr[r] += item
+
+        if r == 0:
+            going_down = True
+        elif r == numRows - 1:
+            going_down = False
+
+        r += 1 if going_down else -1
+        item
+
+    return "".join(lsStr)
 
 
-print(convert("AB", 1))
+responce = convert("PAYPALISHIRING", 3)
+if responce == "PAHNAPLSIIGYIR":
+    print(responce)
+else:
+    print("is wrong")    
+    
