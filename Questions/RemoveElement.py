bbdbc0521ba3c0cc9460d08981bc68a6f57409b3
ali mahmoodi <ali.mahmoodi.heris@gmail.com>
2024-11-15 00:05:41 +0300

    adedd Remove Duplicates from Sorted Array II

--- Questions/RemoveElement.py
+++ Questions/RemoveElement.py
@@ -21,10 +21,30 @@ def removeDuplicates(nums: List[int]) -> int:
         if nums[j] != nums[i]:
             i += 1
             nums[i] = nums[j]
-    return i + 1         
+    return i + 1
 
 
-nums = [0,0,1,1,1,2,2,3,3,4]
-k = removeElement(nums,2)
+def removeDuplicates2(nums: List[int]) -> int:
+    if not nums:
+        return 0
+    write_index = 1
+    count = 1
+    for read_index in range(1, len(nums)):
+        if nums[read_index] == nums[read_index - 1]:
+            count += 1
+        else:
+            count = 1
+
+        if count <= 2:
+            nums[write_index] = nums[read_index]
+            write_index += 1
+    return write_index
+
+
+nums = [0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 3]
+# nums = [1, 1, 1, 2, 2, 3]
+# nums = [1, 1, 1]
+# nums = [0, 0, 0, 0, 0]
+# nums = [1, 2, 2, 2]
+k = removeDuplicates2(nums)
 print(nums[:k])
-print(k)
